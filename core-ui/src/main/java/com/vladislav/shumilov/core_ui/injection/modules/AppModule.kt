6db49d2a076dfc9adfe86862e5c38b7e0e8cd7c6
shumilov <shumilov@simpls.ru>
2020-08-20 17:57:55 +0500

    Start show launch icon placeholder

--- core-ui/src/main/java/com/vladislav/shumilov/core_ui/injection/modules/AppModule.kt
+++ core-ui/src/main/java/com/vladislav/shumilov/core_ui/injection/modules/AppModule.kt
@@ -2,6 +2,10 @@ package com.vladislav.shumilov.core_ui.injection.modules
 
 import android.app.Application
 import android.content.Context
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.imagepipeline.core.ImagePipelineConfig
+import com.facebook.imagepipeline.core.ImageTranscoderType
+import com.facebook.imagepipeline.core.MemoryChunkType
 import com.vladislav.shumilov.core_data.ApplicationContext
 import com.vladislav.shumilov.core_data.ApplicationScope
 import dagger.Module
@@ -9,12 +13,13 @@ import dagger.Provides
 
 @Module
 @ApplicationScope
-class AppModule(private val app : Application) {
+class AppModule(private val app: Application) {
 
-    private val appContext : Context
+    private val appContext: Context
 
     init {
         appContext = app
+        initFresco(appContext)
     }
 
     @Provides
@@ -23,6 +28,17 @@ class AppModule(private val app : Application) {
 
     @Provides
     @ApplicationScope
-    fun provideApp() : Application = app
+    fun provideApp(): Application = app
+
+    private fun initFresco(appContext: Context) {
+        Fresco.initialize(
+            appContext,
+            ImagePipelineConfig.newBuilder(appContext)
+                .setMemoryChunkType(MemoryChunkType.BUFFER_MEMORY)
+                .setImageTranscoderType(ImageTranscoderType.JAVA_TRANSCODER)
+                .experiment().setNativeCodeDisabled(true)
+                .build()
+        )
+    }
 
 }
